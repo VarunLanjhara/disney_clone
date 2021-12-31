ca5b825ebe2950f58ae82e3129985817a7fa19e8
Varun <lanjharav@gmail.com>
2021-12-31 14:05:36 +0530

    Firebase configuration stuff

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHcjHjEkHND4Bow7MOAuXprfztUdB8BN8",
+  authDomain: "disney-clone-9ba58.firebaseapp.com",
+  projectId: "disney-clone-9ba58",
+  storageBucket: "disney-clone-9ba58.appspot.com",
+  messagingSenderId: "1043799158683",
+  appId: "1:1043799158683:web:6a541489efe224e6e77b3e",
+};
+
+const app = !firebase.app.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = firebase.firestore();
+
+export { db };
