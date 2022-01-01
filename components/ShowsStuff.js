70052a78a384cf2cb5fcd99506d7799c926b705f
Varun <lanjharav@gmail.com>
2022-01-01 22:25:28 +0530

    Shows stuff

--- components/ShowsStuff.js
+++ components/ShowsStuff.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import ShowsThumbnail from './ShowsThumbnail'
 
 const ShowsStuff = ({results,title}) => {
     return (
-        <div>
-            <h1>Shows go brr</h1>
-        </div>
+        <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
+      <h2 className="font-semibold">{title}</h2>
+      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
+        {results.map((result) => (
+          <ShowsThumbnail key={result.id} result={result} />
+        ))}
+      </div>
+    </div>
     )
 }
 
