c23e192899a01508e46a57367833962a33c7e87e
Varun <lanjharav@gmail.com>
2022-01-03 12:10:12 +0530

    Trailer stuff

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getSession, useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import Head from "next/head";
@@ -16,6 +16,7 @@ const Movie = ({ result }) => {
       router.push("/");
     }
   }, []);
+  const [showPlayer, setShowPlayer] = useState(false);
   return (
     <div className="relative">
       <Head>
@@ -79,6 +80,24 @@ const Movie = ({ result }) => {
               {result.genres.map((genre) => genre.name + " ")}{" "}
             </p>
             <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
+          </div>
+          {showPlayer && (
+            <div className="absolute inset-0 bg-black opacity-50 h-full w-full z-50"></div>
+          )}
+          <div
+            className={`absolute top-3 inset-x-[7%] md:inset-x-[13%] rounded overflow-hidden transition duration-1000 ${
+              showPlayer ? "opacity-100 z-50" : "opacity-0"
+            }`}
+          >
+            <div className="flex items-center justify-between bg-black text-[#f9f9f9] p-3.5">
+              <span className="font-semibold">Play Trailer</span>
+              <div
+                className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 hover:bg-[#0F0F0F]"
+                onClick={() => setShowPlayer(false)}
+              >
+                <XIcon className="h-5" />
+              </div>
+            </div>
           </div>
         </section>
       )}
